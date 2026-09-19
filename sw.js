add9e1af349c21e1e074edeb90c11e6eb5a2aa5b
tatuya2312-ui <tatuya2312@gmail.com>
2026-09-20 02:49:14 +0900

    Refresh RecipeBook web cache v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="recipebook-v2";
+const CACHE="recipebook-v3";
 const SHELL=["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
