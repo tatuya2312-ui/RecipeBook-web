90372fc7f6a6f1d60a4d8a91f65dd85fc4cda5de
tatuya2312-ui <tatuya2312@gmail.com>
2026-09-20 02:36:56 +0900

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE="recipebook-v1";
+const SHELL=["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+      return r;
+    }).catch(()=>caches.match(e.request))
+  );
+});
