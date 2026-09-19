38a19bfe4a231130767f26676b8dfdad2d536de9
tatuya2312-ui <tatuya2312@gmail.com>
2026-09-20 03:00:13 +0900

    Clear old RecipeBook caches

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-const CACHE="recipebook-v4";
+const CACHE="recipebook-v5";
 const SHELL=["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys()
+    .then(keys=>Promise.all(keys.filter(k=>k.startsWith("recipebook-")&&k!==CACHE).map(k=>caches.delete(k))))
+    .then(()=>self.clients.claim())
+));
 self.addEventListener("fetch",e=>{
   if(e.request.method!=="GET") return;
   e.respondWith(
